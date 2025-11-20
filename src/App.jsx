365dbe932dd0f34589aef6ca22fab9900651d391
Lucas Laino <lucaslaino00@gmail.com>
2025-11-19 21:16:18 -0300

    feat(banner): Adiciona o componente Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
 
 export default function App() {
     return (
-        <Header />
+        <>
+            <Header />
+            <Banner />
+        </>
+
     )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,7 @@
+import './banner.scss';
+
+export default function Banner() {
+    return (
+        <div className="banner"></div>
+    )
+}
