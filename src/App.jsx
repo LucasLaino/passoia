5fbaee6237238626eb1f5a625d960f24cbc8169f
Lucas Laino <lucaslaino00@gmail.com>
2025-11-19 21:21:24 -0300

    feat(Maquiagens): Adiciona o componente Maquiagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner";
+import Maquiagens from './components/Maquiagens/Maquiagens'
 
 export default function App() {
     return (
         <>
             <Header />
             <Banner />
+            <Maquiagens />
         </>
 
     )
---
+++ src/components/Maquiagens/Maquiagens.jsx
@@ -0,0 +1,22 @@
+import './Maquiagens.scss';
+import cardLabiosImg from '../../assets/images/card-labios.jpg'
+import cardOlhosImg from '../../assets/images/card-olhos.jpg'
+import cardRostoImg from '../../assets/images/card-rosto.jpg'
+import cardTendenciaImg from '../../assets/images/card-tendencia.jpg'
+
+export default function Maquiagens() {
+    return (
+        <>
+            <article className='categorias-maquiagens'>
+                <h2>LOOKS E DICAS DE MAQUIAGEM</h2>
+
+                <section className='categorias'>
+                    <img src={cardLabiosImg} alt="card-labios-img" />
+                    <img src={cardOlhosImg} alt="card-labios-img" />
+                    <img src={cardRostoImg} alt="card-rosto-img" />
+                    <img src={cardTendenciaImg} alt="card-tendencia-img" />
+                </section>
+            </article>
+        </>
+    )
+}
